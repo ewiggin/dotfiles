876fc62c43e9ab0d06c37639a7f29cf7ffe6338c
Mario Martinez <mmartinez@energy-tools.net>
2022-05-18 21:14:33 +0200

    fix dot.sh

--- bin/dot.sh
+++ bin/dot.sh
@@ -5,10 +5,10 @@ set -euo pipefail
 # TODO: select system (mac | linux)
 
 source "$DOTFILES_PATH/mac/brew/brew.sh"
-source "$DOTFILES_PATH/configure-mac.sh"
 source "$DOTFILES_PATH/projects/download-projects.sh"
-source "$DOTFILES_PATH/docker/install.sh"
 source "$DOTFILES_PATH/java/install.sh"
+source "$DOTFILES_PATH/docker/install.sh"
+source "$DOTFILES_PATH/mac/configure-mac.sh"
 
 # Move configurations to ~/.config
 mv $DOTFILES_PATH/configs ~/.config
